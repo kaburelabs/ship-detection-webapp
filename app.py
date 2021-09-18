a0f5f913270afb4ec5a36bae38f5edeecde330ef
Leonardo Ferreira <ferreira_master@hotmail.com>
2021-09-17 21:50:28 -0300

    testing pytorch model again

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import dash_bootstrap_components as dbc
 import plotly_express as px
 
 from components.navbar import navbar
-# from components.segmentation_model import loading_image_and_fig, pytorch_segmentation
+from components.segmentation_model import loading_image_and_fig, pytorch_segmentation
 
 import pandas as pd
 import json
@@ -223,108 +223,108 @@ app.layout = html.Div(
 )
 
 
-# @app.callback(
-#     Output("has-ship-on-image", "children"),
-#     Output("original-image-renderer", "figure"),
-#     Output("loaded-image-name", "children"),
-#     Output("image-df-data", "data"),
-#     Input("load-img", "n_clicks"),
-#     State("pick-image-folder-radio", "value"),
-# )
-# def starting_the_app(btn_n, images_to_load):
+@app.callback(
+    Output("has-ship-on-image", "children"),
+    Output("original-image-renderer", "figure"),
+    Output("loaded-image-name", "children"),
+    Output("image-df-data", "data"),
+    Input("load-img", "n_clicks"),
+    State("pick-image-folder-radio", "value"),
+)
+def starting_the_app(btn_n, images_to_load):
 
-#     txt, img_fig, validation_df = loading_image_and_fig(images_to_load)
+    txt, img_fig, validation_df = loading_image_and_fig(images_to_load)
 
-#     return (
-#         txt,
-#         img_fig,
-#         f"{validation_df[0]['ImageId']}",
-#         validation_df,
-#     )
+    return (
+        txt,
+        img_fig,
+        f"{validation_df[0]['ImageId']}",
+        validation_df,
+    )
 
-# @app.callback(
-#     Output("ground-truth-image", "children"),
-#     Output("output-image", "children"),
-#     Input("predict-btn", "n_clicks"),
-#     State("image-df-data", "data"),
-# )
-# def starting_the_app(n_clicks, content):
+@app.callback(
+    Output("ground-truth-image", "children"),
+    Output("output-image", "children"),
+    Input("predict-btn", "n_clicks"),
+    State("image-df-data", "data"),
+)
+def starting_the_app(n_clicks, content):
 
-#     if n_clicks == None:
-#         raise PreventUpdate
+    if n_clicks == None:
+        raise PreventUpdate
 
-#     time.sleep(2.5)
+    time.sleep(2.5)
 
-#     img, mask, output = pytorch_segmentation(pd.DataFrame.from_dict(content))
+    img, mask, output = pytorch_segmentation(pd.DataFrame.from_dict(content))
 
-#     mask_fig = px.imshow(mask)
+    mask_fig = px.imshow(mask)
 
-#     mask_fig.update_layout(
-#         margin=dict(t=0, b=0, r=0, l=0),
-#         height=300,
-#         width=300,
-#         xaxis=dict(ticks=None),
-#         plot_bgcolor="rgba(0,0,0,0)",
-#         paper_bgcolor="rgba(0,0,0,0)",
-#         dragmode="drawrect",
-#         newshape=dict(line_color="cyan"),
-#     )
+    mask_fig.update_layout(
+        margin=dict(t=0, b=0, r=0, l=0),
+        height=300,
+        width=300,
+        xaxis=dict(ticks=None),
+        plot_bgcolor="rgba(0,0,0,0)",
+        paper_bgcolor="rgba(0,0,0,0)",
+        dragmode="drawrect",
+        newshape=dict(line_color="cyan"),
+    )
 
-#     output_fig = px.imshow(output)
-#     output_fig.update_layout(
-#         margin=dict(t=0, b=0, r=0, l=0),
-#         height=300,
-#         width=300,
-#         xaxis=dict(ticks=None),
-#         plot_bgcolor="rgba(0,0,0,0)",
-#         paper_bgcolor="rgba(0,0,0,0)",
-#         dragmode="drawrect",
-#         newshape=dict(line_color="cyan"),
-#     )
+    output_fig = px.imshow(output)
+    output_fig.update_layout(
+        margin=dict(t=0, b=0, r=0, l=0),
+        height=300,
+        width=300,
+        xaxis=dict(ticks=None),
+        plot_bgcolor="rgba(0,0,0,0)",
+        paper_bgcolor="rgba(0,0,0,0)",
+        dragmode="drawrect",
+        newshape=dict(line_color="cyan"),
+    )
 
-#     return (
-#         dcc.Graph(
-#             figure=mask_fig,
-#             config={
-#                 "modeBarButtonsToAdd": [
-#                     "drawline", "drawopenpath", "drawclosedpath",
-#                     "drawcircle", "drawrect", "eraseshape" ]
-#             },
-#         ),
-#         dcc.Graph(
-#             figure=output_fig,
-#             config={
-#                 "modeBarButtonsToAdd": [
-#                     "drawline", "drawopenpath", "drawclosedpath",
-#                     "drawcircle", "drawrect", "eraseshape"]
-#             },
-#         ),
-#     )
-
-
-# @app.callback(
-#     Output("webapp-started", "data"),
-#     Input("webapp-starter", "data"),
-#     State("loaded-image-name", "children"),
-# )
-# def starting_the_app(app_starter, content):
-
-#     if app_starter["status"] == "Starting" and content == None:
-#         return {"status": "started"}
-#     else:
-#         return dash.no_update
+    return (
+        dcc.Graph(
+            figure=mask_fig,
+            config={
+                "modeBarButtonsToAdd": [
+                    "drawline", "drawopenpath", "drawclosedpath",
+                    "drawcircle", "drawrect", "eraseshape" ]
+            },
+        ),
+        dcc.Graph(
+            figure=output_fig,
+            config={
+                "modeBarButtonsToAdd": [
+                    "drawline", "drawopenpath", "drawclosedpath",
+                    "drawcircle", "drawrect", "eraseshape"]
+            },
+        ),
+    )
 
 
-# @app.callback(
-#     Output("output-results", "style"),
-#     Input("predict-btn", "n_clicks")
-# )
-# def starting_the_app(predict_btn):
+@app.callback(
+    Output("webapp-started", "data"),
+    Input("webapp-starter", "data"),
+    State("loaded-image-name", "children"),
+)
+def starting_the_app(app_starter, content):
 
-#     if predict_btn == None:
-#         return {"display":"none"}
-#     else:
-#         return {"display":"block"}
+    if app_starter["status"] == "Starting" and content == None:
+        return {"status": "started"}
+    else:
+        return dash.no_update
+
+
+@app.callback(
+    Output("output-results", "style"),
+    Input("predict-btn", "n_clicks")
+)
+def starting_the_app(predict_btn):
+
+    if predict_btn == None:
+        return {"display":"none"}
+    else:
+        return {"display":"block"}
 
 
 if __name__ == "__main__":
